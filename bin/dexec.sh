8bf1e778a8dd97852bd5b32a913f18973537411e
reuveniy <reuveniy@yny.co.il>
2015-11-22 23:51:16 -0500

    fixing regex encoding

--- bin/dexec.sh
+++ bin/dexec.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 filter="$1"
+ftext="$(echo $filter | sed -e 's/[\/&]/\\&/g')"
 shift
 echo Filtering $filter
 docker ps | grep $filter | while read d; do
   ID="$(echo $d | cut -f 1 -d ' ')"
   echo $filter:$ID# $@
-  { docker exec $ID $@ | sed -e "s/^/$filter:$ID /"; } &
+  { docker exec $ID $@ | sed -e "s/^/$ftext:$ID /"; } &
 done
 wait
 sleep 1
